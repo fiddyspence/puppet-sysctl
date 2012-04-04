033df025909fe37c942bee0e560cb86ee0d56989
fiddyspence <chris@spence.org.uk>
2012-04-04 18:36:55 +0000

    moooooooooooooo

--- lib/puppet/provider/sysctl/sysctl.rb
+++ lib/puppet/provider/sysctl/sysctl.rb
@@ -15,14 +15,41 @@ Puppet::Type.type(:sysctl).provide(:sysctl) do
     instances = []
     sysctloutput = sysctl('-a')
     sysctloutput.each do |line|
+      next if line =~ /dev.cdrom.info/
       if line =~ /=/
-        #next if line =~ /dev\.cdrom.\info/
         kernelsetting = line.split('=')
         instances << new(:name => kernelsetting[0].strip, :value => kernelsetting[1].strip)
-        puts instances.inspect
       end
     end
     instances
+  end
+
+  def permanent
+    lines.find do |line|
+      if line =~ /^#{resource[:name]}/
+        return true
+      end
+    end
+    false
+  end
+
+  def destroy
+    local_lines = lines
+    File.open(resource[:path],'w') do |fh|
+      fh.write(local_lines.reject{|l| l =~ /^#{resource[:name]}\s?\=\s?[\S+]$/ }.join(''))
+    end
+  end
+
+  def permanent=(ispermanent)
+    if ispermanent
+        a = permanent
+        b = ( resource[:value] == nil ? value : resource[:value] ) 
+        if a == false
+          File.open(resource[:path], 'a') do |fh|
+            fh.puts "#{resource[:name]} = #{b}"
+          end
+        end
+    end
   end
 
   def value
@@ -32,6 +59,20 @@ Puppet::Type.type(:sysctl).provide(:sysctl) do
 
   def value=(thesetting)
     sysctl('-w', "#{resource[:name]}=#{thesetting}")
+    if resource[:permanent]
+      #do some permanence shit here
+    end
   end
 
+  private
+  def lines
+    @lines ||= File.readlines(resource[:path])
+  end
+
+#  def create
+#    File.open(resource[:path], 'a') do |fh|
+#      fh.puts resource[:line]
+#    end
+#  end
+#
 end
