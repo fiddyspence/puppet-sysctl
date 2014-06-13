7bba83e5419bdd198464c9f8dcc98735e34548b5
fiddyspence <chris@spence.org.uk>
2014-06-13 09:53:50 +0100

    Adding new puppet3 provider with a confine on the old code for puppet 2

--- lib/puppet/provider/sysctl/linux3.rb
+++ lib/puppet/provider/sysctl/linux3.rb
@@ -1,8 +1,28 @@
-Puppet::Type.type(:sysctl).provide(:linux) do
+Puppet::Type.type(:sysctl).provide(:linux3) do
 
   confine  :kernel => 'linux'
-  confine  :true   => Puppet.version =~ /^2/
-  commands :sysctl => 'sysctl'
+
+
+  class CommandDefinerNoMerge < Puppet::Provider::CommandDefiner
+    def command
+      @confiner.confine :exists => @path, :for_binary => true
+      Puppet::Provider::Command.new(@name, @path, Puppet::Util, Puppet::Util::Execution, { :failonfail => true, :combine => false, :custom_environment => @custom_environment })
+    end
+  end
+ 
+  def self.has_nomerge_command(name, path, &block)
+    name = name.intern
+    command = CommandDefinerNoMerge.define(name, path, self, &block)
+
+    @commands[name] = command.executable
+
+    create_class_and_instance_method(name) do |*args|
+      return command.execute(*args)
+    end
+  end
+
+  has_nomerge_command(:sysctl, 'sysctl') do
+  end
 
   def exists?
     @property_hash[:ensure] == :present
