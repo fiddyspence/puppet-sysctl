a7be349bb7d09babb970328328c0af7153a7f081
fiddyspence <chris@spence.org.uk>
2015-02-12 14:07:49 +0000

    renaming the target module for acceptance tests to sysctl

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -22,7 +22,7 @@ RSpec.configure do |c|
   # Configure all nodes in nodeset
   c.before :suite do
     hosts.each do |host|
-      copy_module_to(host, :source => proj_root, :module_name => 'concat')
+      copy_module_to(host, :source => proj_root, :module_name => 'sysctl')
     end
   end
 
